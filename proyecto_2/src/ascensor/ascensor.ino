f35c810a2e769895d476ee941d6a88562e6cd585
Daniel Z <danyazunigab@gmail.com>
2026-06-05 15:04:43 -0600

    feat(ino): version final

--- proyecto_2/src/ascensor/ascensor.ino
+++ proyecto_2/src/ascensor/ascensor.ino
@@ -4,9 +4,11 @@ volatile long encoderCount = 0;
 
 // Posiciones de pisos
 
-const long PISO1 = 0;
-const long PISO2 = 5000;
-const long PISO3 = 10000;
+const long PISO1 = 10;
+const long PISO2 = 105;
+const long PISO3 = 195;
+const long PISO4 = 285;
+const long PISO5 = 380;
 
 // Setpoint
 
@@ -14,28 +16,34 @@ long setpoint = PISO1;
 
 // Home
 
-#define HOME_SWITCH 12
+#define TRIG_PIN 13
+#define ECHO_PIN A0
+
+const float HOME_DISTANCE_CM = 1.0;
+const float HOME_TOLERANCE_CM = 1.0;
 
 // L298N
 
 #define ENA 5
-#define IN1 6
-#define IN2 7
+#define IN1 7
+#define IN2 6
 
 // Botones
 
 #define BTN1 8
 #define BTN2 9
 #define BTN3 10
+#define BTN4 11
+#define BTN5 12
 
 // Encoder
 
 #define ENCODER_A 2
 #define ENCODER_B 3
 
-float Kp = 0.10;
+float Kp = 0.18;
 float Ki = 0.0005;
-float Kd = 0.20;
+float Kd = 0.45;
 
 float integral = 0;
 float prevError = 0;
@@ -53,6 +61,71 @@ void encoderISR()
   }
 }
 
+float leerDistancia()
+{
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(TRIG_PIN, LOW);
+
+    long duracion =
+        pulseIn(
+            ECHO_PIN,
+            HIGH,
+            25000
+        );
+
+    if(duracion == 0)
+        return 999;
+
+    return duracion * 0.0343 / 2.0;
+}
+
+void actualizarHome()
+{
+    static uint8_t contador = 0;
+
+    float distancia =
+        leerDistancia();
+
+    bool enHome =
+        abs(
+            distancia -
+            HOME_DISTANCE_CM
+        ) < HOME_TOLERANCE_CM;
+
+    if(enHome)
+    {
+        contador++;
+
+        if(contador >= 3)
+        {
+            noInterrupts();
+            encoderCount = 0;
+            interrupts();
+            detenerMotor();
+
+            contador = 3;
+        }
+    }
+    else
+    {
+        contador = 0;
+    }
+}
+bool estaEnHome()
+{
+    float distancia = leerDistancia();
+
+    return abs(
+        distancia -
+        HOME_DISTANCE_CM
+    ) < HOME_TOLERANCE_CM;
+}
+
 void subirMotor(int pwm)
 {
   digitalWrite(IN1, HIGH);
@@ -63,10 +136,16 @@ void subirMotor(int pwm)
 
 void bajarMotor(int pwm)
 {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
+    if(estaEnHome())
+    {
+        detenerMotor();
+        return;
+    }
 
-  analogWrite(ENA, pwm);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+
+    analogWrite(ENA, pwm);
 }
 
 void detenerMotor()
@@ -87,23 +166,14 @@ void leerBotones()
 
   if (!digitalRead(BTN3))
     setpoint = PISO3;
+
+  if (!digitalRead(BTN4))
+    setpoint = PISO4;
+
+  if (!digitalRead(BTN5))
+    setpoint = PISO5;
 }
 
-void homing()
-{
-  bajarMotor(80);
-
-  while (digitalRead(HOME_SWITCH))
-  {
-    delay(1);
-  }
-
-  detenerMotor();
-
-  encoderCount = 0;
-
-  delay(300);
-}
 
 float calcularPID()
 {
@@ -133,97 +203,63 @@ float calcularPID()
 
 void controlarAscensor()
 {
-  long error =
-      setpoint - encoderCount;
+    long error =
+        setpoint - encoderCount;
 
-  long distancia =
-      abs(error);
+    long distancia =
+        abs(error);
 
-  if (distancia < 50)
-  {
-    detenerMotor();
-
-    integral = 0;
-
-    return;
-  }
-
-  //--------------------------------
-  // MUY LEJOS
-  //--------------------------------
-
-  if (distancia > 2000)
-  {
-    if (error > 0)
-      subirMotor(255);
-    void loop()
+    if(distancia < 3)
     {
-      leerBotones();
+        detenerMotor();
 
-      controlarAscensor();
+        integral = 0;
 
-      static unsigned long t = 0;
-
-      if (millis() - t > 100)
-      {
-        t = millis();
-
-        Serial.print("POS: ");
-
-        Serial.print(
-            encoderCount);
-
-        Serial.print(" SP: ");
-
-        Serial.print(
-            setpoint);
-
-        Serial.print(" ERR: ");
-
-        Serial.println(
-            setpoint -
-            encoderCount);
-      }
+        return;
     }
-    else bajarMotor(255);
 
-    return;
-  }
-
-  //--------------------------------
-  // DISTANCIA MEDIA
-  //--------------------------------
-
-  if (distancia > 500)
-  {
-    if (error > 0)
-      subirMotor(180);
+if(distancia > 80)
+{
+    if(error > 0)
+        subirMotor(240);
     else
-      bajarMotor(180);
+        bajarMotor(240);
 
     return;
-  }
+}
 
-  //--------------------------------
-  // PID FINO
-  //--------------------------------
+if(distancia > 20)
+{
+    if(error > 0)
+        subirMotor(140);
+    else
+        bajarMotor(140);
 
-  float control =
-      calcularPID();
+    return;
+}
 
-  int pwm =
-      abs(control);
+    //--------------------------------
+    // PID FINO
+    //--------------------------------
 
-  pwm =
-      constrain(
-          pwm,
-          70,
-          200);
+    float control =
+        calcularPID();
 
-  if (control > 0)
-    subirMotor(pwm);
-  else
-    bajarMotor(pwm);
+    int pwm =
+        abs(control);
+
+    pwm =
+        constrain(
+            pwm,
+            140,
+            255
+        );
+
+    if(control > 0)
+        subirMotor(pwm);
+    else
+        bajarMotor(pwm);
+
 }
 
 void setup()
@@ -235,9 +271,20 @@ void setup()
   pinMode(BTN1, INPUT_PULLUP);
   pinMode(BTN2, INPUT_PULLUP);
   pinMode(BTN3, INPUT_PULLUP);
+  pinMode(BTN4, INPUT_PULLUP);
+  pinMode(BTN5, INPUT_PULLUP);
 
-  pinMode(HOME_SWITCH,
-          INPUT_PULLUP);
+pinMode(
+    TRIG_PIN,
+    OUTPUT
+);
+
+pinMode(
+    ECHO_PIN,
+    INPUT
+);
+
+ digitalWrite(TRIG_PIN, LOW);
 
   pinMode(ENCODER_A,
           INPUT_PULLUP);
@@ -252,36 +299,54 @@ void setup()
       CHANGE);
 
   Serial.begin(115200);
+    Serial.println("Ascensor iniciado");
 
-  homing();
+    // Si al arrancar ya está en el piso 1,
+    // sincronizamos inmediatamente
+    float distancia = leerDistancia();
+
+    if(abs(distancia - HOME_DISTANCE_CM) < HOME_TOLERANCE_CM)
+    {
+        noInterrupts();
+        encoderCount = 0;
+        interrupts();
+
+        Serial.println("HOME detectado");
+    }
 }
 
 void loop()
 {
-  leerBotones();
-
-  controlarAscensor();
-
-  static unsigned long t = 0;
-
-  if (millis() - t > 100)
-  {
-    t = millis();
-
-    Serial.print("POS: ");
-
-    Serial.print(
-        encoderCount);
-
-    Serial.print(" SP: ");
-
-    Serial.print(
-        setpoint);
-
-    Serial.print(" ERR: ");
-
-    Serial.println(
-        setpoint -
-        encoderCount);
-  }
+//    actualizarHome();
+//
+//    leerBotones();
+//
+//    controlarAscensor();
+//
+//  static unsigned long t = 0;
+//
+//  if (millis() - t > 100)
+//  {
+//    t = millis();
+//
+//    Serial.print("POS: ");
+//
+//    Serial.print(
+//        encoderCount);
+//
+//    Serial.print(" SP: ");
+//
+//    Serial.print(
+//        setpoint);
+//
+//    Serial.print(" ERR: ");
+//
+//    Serial.println(
+//        setpoint -
+//        encoderCount);
+//  }
+detenerMotor();
+delay(2000);
+Serial.print(encoderCount);
+subirMotor(255);
 }
